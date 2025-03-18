ad405489b785eeeea7e5b46dd38b9dca54feeb8e
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2025-03-19 04:34:32 +0530

    Removed a Unwanted File

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,9 +1,8 @@
 import express from 'express'
-import { validateSchema } from '../middleware/inputValidator.js';
 import { generateAgeWiseDisributionReport } from '../controller/csvToJsonController.js';
 
 const router = express.Router();
 
-router.get("/read-csv-and-add-to-database" , generateAgeWiseDisributionReport)
+router.get("/read-csv-and-add-to-database" ,  generateAgeWiseDisributionReport)
 
 export default router
